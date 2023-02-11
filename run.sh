b99726c03f2f2623baec86a67e02f8e9a3c5e360
HsiehTing Lin <hsieh.ting.lin@gmail.com>
2023-02-12 03:35:43 +0800

    add new prompt

--- run.sh
+++ run.sh
@@ -18,7 +18,23 @@ for filename in "$path"/*.yaml; do
 done
 
 # Convert the combined YAML file to JSON
-cat "$output_file" | python -c "import sys, yaml, json; print(json.dumps(yaml.load(sys.stdin, Loader=yaml.FullLoader), indent=2))" > "my_prompts.json"
+cat "$output_file" | python -c "import sys, yaml, json; print(json.dumps(yaml.load(sys.stdin, Loader=yaml.FullLoader), indent=2))" > "user_custom.json"
+
+file_to_replace="user_custom.json"
+target_dir="$HOME/.chatgpt/cache_model"
+
+if [ -f "$target_dir/$file_to_replace" ]; then
+  rm "$target_dir/$file_to_replace"
+  echo "Removed old file: $target_dir/$file_to_replace"
+fi
+
+new_file="./$file_to_replace"
+if [ -f "$new_file" ]; then
+  cp "$new_file" "$target_dir/$file_to_replace"
+  echo "Replaced with new file: $target_dir/$file_to_replace"
+else
+  echo "New file not found: $new_file"
+fi
 
 # Display a message to indicate that the conversion is complete
 echo "Done. 🟢 "
